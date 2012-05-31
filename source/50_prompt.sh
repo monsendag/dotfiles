728c7abfe0d1bd6d16326e49c2af6758755c7b3f
Dag Einar Monsen <monsen.dag@gmail.com>
2012-05-31 13:56:38 +0200

    modified prompt colors

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -22,34 +22,61 @@
 # 32  42  green     36  46  cyan
 # 33  43  yellow    37  47  white
 
-if [[ ! "${prompt_colors[@]}" ]]; then
-  prompt_colors=(
-    "36" # information color
-    "37" # bracket color
-    "31" # error color
-  )
+ NONE="\[\033[0m\]"    # unsets color to term's fg color
 
-  if [[ "$SSH_TTY" ]]; then
-    # connected via ssh
-    prompt_colors[0]="32"
-  elif [[ "$USER" == "root" ]]; then
-    # logged in as root
-    prompt_colors[0]="35"
-  fi
-fi
+# regular colors
+ K="\[\033[0;30m\]"    # black
+ R="\[\033[0;31m\]"    # red
+ G="\[\033[0;32m\]"    # green
+ Y="\[\033[0;33m\]"    # yellow
+ B="\[\033[0;34m\]"    # blue
+ M="\[\033[0;35m\]"    # magenta
+ C="\[\033[0;36m\]"    # cyan
+ W="\[\033[0;37m\]"    # white
 
-# Inside a prompt function, run this alias to setup local $c0-$c9 color vars.
-alias prompt_getcolors='prompt_colors[9]=; local i; for i in ${!prompt_colors[@]}; do local c$i="\[\e[0;${prompt_colors[$i]}m\]"; done'
+# emphasized (bolded) colors
+ EMK="\[\033[1;30m\]"
+ EMR="\[\033[1;31m\]"
+ EMG="\[\033[1;32m\]"
+ EMY="\[\033[1;33m\]"
+ EMB="\[\033[1;34m\]"
+ EMM="\[\033[1;35m\]"
+ EMC="\[\033[1;36m\]"
+ EMW="\[\033[1;37m\]"
+
+# background colors
+ BGK="\[\033[40m\]"
+ BGR="\[\033[41m\]"
+ BGG="\[\033[42m\]"
+ BGY="\[\033[43m\]"
+ BGB="\[\033[44m\]"
+ BGM="\[\033[45m\]"
+ BGC="\[\033[46m\]"
+ BGW="\[\033[47m\]"
+
+pwd_maxlen() {
+    # How many characters of the $PWD should be kept
+    local pwdmaxlen=30
+    # Indicate that there has been dir truncation
+    local trunc_symbol="..."
+    local dir=${PWD##*/}
+    pwdmaxlen=$(((pwdmaxlen < ${#dir}) ? ${#dir} : pwdmaxlen))
+   	path=${PWD/#$HOME/\~}
+    local pwdoffset=$((${#path} - pwdmaxlen))
+    if [ ${pwdoffset} -gt "0" ]
+    then
+        path=${path:$pwdoffset:$pwdmaxlen}
+        path=${trunc_symbol}/${path#*/}
+    fi
+}
 
 # Exit code of previous command.
 function prompt_exitcode() {
-  prompt_getcolors
-  [[ $1 != 0 ]] && echo " $c2$1$c9"
+  [[ $1 != 0 ]] && echo " $R$1$c9"
 }
 
 # Git status.
 function prompt_git() {
-  prompt_getcolors
   local status output flags
   status="$(git status 2>/dev/null)"
   [[ $? != 0 ]] && return;
@@ -64,20 +91,19 @@ function prompt_git() {
       END {print r}'
   )"
   if [[ "$flags" ]]; then
-    output="$output$c1:$c0$flags"
+    output="$Y$output$W:$Y$flags"
   fi
-  echo "$c1[$c0$output$c1]$c9"
+  echo "$W[$output$W]$NONE"
 }
 
 # SVN info.
 function prompt_svn() {
-  prompt_getcolors
   local info="$(svn info . 2> /dev/null)"
   local last current
   if [[ "$info" ]]; then
     last="$(echo "$info" | awk '/Last Changed Rev:/ {print $4}')"
     current="$(echo "$info" | awk '/Revision:/ {print $2}')"
-    echo "$c1[$c0$last$c1:$c0$current$c1]$c9"
+    echo "$W[$G$last$G:$Y$current$W]$NONE"
   fi
 }
 
@@ -86,32 +112,40 @@ prompt_stack=()
 trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
 
 function prompt_command() {
-  local exit_code=$?
-  # If the first command in the stack is prompt_command, no command was run.
-  # Set exit_code to 0 and reset the stack.
-  [[ "${prompt_stack[0]}" == "prompt_command" ]] && exit_code=0
-  prompt_stack=()
+	pwd_maxlen
 
-  # While the simple_prompt environment var is set, disable the awesome prompt.
-  [[ "$simple_prompt" ]] && PS1='\n$ ' && return
+	USERCOLOR=$M
+	if [[ "$SSH_TTY" ]]; then # connected via ssh
+		USERCOLOR=$Y
+	elif [[ "$USER" == "root" ]]; then # logged in as root
+		USERCOLOR=$G
+	fi
 
-  prompt_getcolors
-  # http://twitter.com/cowboy/status/150254030654939137
-  PS1="\n"
-  # svn: [repo:lastchanged]
-  PS1="$PS1$(prompt_svn)"
-  # git: [branch:flags]
-  PS1="$PS1$(prompt_git)"
-  # misc: [cmd#:hist#]
-  # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
-  # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
-  PS1="$PS1\n"
-  # date: [HH:MM:SS]
-  PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
-  # exit code: 127
-  PS1="$PS1$(prompt_exitcode "$exit_code")"
-  PS1="$PS1 \$ "
+	local exit_code=$?
+	# If the first command in the stack is prompt_command, no command was run.
+	# Set exit_code to 0 and reset the stack.
+	[[ "${prompt_stack[0]}" == "prompt_command" ]] && exit_code=0
+	prompt_stack=()
+
+	# While the simple_prompt environment var is set, disable the awesome prompt.
+	[[ "$simple_prompt" ]] && PS1='\n$ ' && return
+
+	# init PS1 with a newline
+	PS1=""
+	# svn: [repo:lastchanged]
+	PS1="$PS1$(prompt_svn)"
+	# git: [branch:flags]
+	PS1="$PS1$(prompt_git)"
+	# misc: [cmd#:hist#]
+	# PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
+	# path: [user@host:path]
+	PS1="$PS1$W[$USERCOLOR\u$W@$C\h$W:$R$path$W]$NONE"
+	PS1="$PS1\n"
+	# date: [HH:MM:SS]
+	# PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+	# exit code: 127
+	PS1="$PS1$(prompt_exitcode "$exit_code")"
+	PS1="$PS1 \$ "
 }
 
 PROMPT_COMMAND="prompt_command"
