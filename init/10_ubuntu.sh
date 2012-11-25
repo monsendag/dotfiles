bdd2c5d9f7d54900d0b587074a68f6559d1cf25e
Dag Einar Monsen <EMAIL>
2012-11-25 16:32:47 +1000

    Don't add sudoers-dotfiles. It breaks sudo on EC2.

--- init/10_ubuntu.sh
+++ init/10_ubuntu.sh
@@ -1,23 +1,6 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
 
-# If the old files isn't removed, the duplicate APT alias will break sudo!
-sudoers_old="/etc/sudoers.d/sudoers-cowboy"; [[ -e "$sudoers_old" ]] && sudo rm "$sudoers_old"
-
-# Installing this sudoers file makes life easier.
-sudoers_file="sudoers-dotfiles"
-sudoers_src=~/.dotfiles/conf/ubuntu/$sudoers_file
-sudoers_dest="/etc/sudoers.d/$sudoers_file"
-if [[ ! -e "$sudoers_dest" || "$sudoers_dest" -ot "$sudoers_src" ]]; then
-  e_header "Updating sudoers"
-  visudo -cf "$sudoers_src" >/dev/null && {
-    sudo cp "$sudoers_src" "$sudoers_dest" &&
-    sudo chmod 0440 "$sudoers_dest"
-  } >/dev/null 2>&1 &&
-  echo "File $sudoers_dest updated." ||
-  echo "Error updating $sudoers_dest file."
-fi
-
 # Update APT.
 e_header "Updating APT"
 sudo apt-get -qq update
