71372850669881d9f39bd41dc277a5e97156c720
Dag Einar Monsen <me@dag.im>
2013-01-17 16:48:22 +1100

    Github API tokens are no longer supported.

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -10,8 +10,4 @@ SSH Keys
  1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
  2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
-Git / GitHub
- 1. Get GitHub API token from https://github.com/account/admin
- 2. (here) git config --global github.token "omgsupersecretgithubapitoken"
- 3. (here) git config --global user.email "email@domain.com"
 EOF
