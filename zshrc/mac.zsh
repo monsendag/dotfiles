a48633b44ed0060f66a7f1d619f399e86c986647
Dag Einar Monsen <me@dag.im>
2014-02-26 18:04:39 +0100

    Remove locale declaration for macs.

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -1,8 +1,5 @@
 export JAVA_HOME=$(/usr/libexec/java_home)
 
-export LC_ALL=no_NO.UTF-8;
-export LANG=no_NO.UTF-8;
-
 alias mou='open -a Mou'
 alias subl='Open -a "Sublime Text 2"'
 alias br='Open -a "Brackets"'
