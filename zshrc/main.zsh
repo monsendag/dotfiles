83bb70f0fb2a6c432e722cecbefccd546560f496
Dag Einar Monsen <me@dag.im>
2014-03-03 22:24:48 +0100

    Move imports to bottom of main.zsh.

--- zshrc/main.zsh
+++ zshrc/main.zsh
@@ -1,17 +1,8 @@
-ZSHRC="$HOME/.dotfiles/zshrc";
 
-# OS specific code
-[ `uname` '==' "Linux" ] && source "$ZSHRC/linux.zsh";
-[ `uname` '==' "Darwin" ] && source "$ZSHRC/mac.zsh";
+ZSHRC="$HOME/.dotfiles/zshrc";
 
 # independent environment variables 
 [ -f "$HOME/.environment" ] && source "$HOME/.environment"; 
-
-# oh-my-zsh
-source $ZSHRC/oh-my-zsh.zsh
-# pure theme
-source $ZSHRC/pure.zsh
-
 # set ls colors
 eval `dircolors $HOME/.dircolors`
 
@@ -28,4 +19,14 @@ export WORKON_HOME=$HOME/.virtualenvs
 autoload zmv
 alias zmz='noglob zmv'
 alias zcp='noglob zmv -C'
+
+# oh-my-zsh
+source $ZSHRC/oh-my-zsh.zsh
+# pure theme
+source $ZSHRC/pure.zsh
+
+# OS specific code
+[ `uname` '==' "Linux" ] && source "$ZSHRC/linux.zsh";
+[ `uname` '==' "Darwin" ] && source "$ZSHRC/mac.zsh";
+
 
