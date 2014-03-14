4922ba3380cff18f1694a57fbffcaf82f2c107d8
Dag Einar Monsen <me@dag.im>
2014-03-14 20:37:18 +0100

    Reorganize main.zsh

--- zshrc/main.zsh
+++ zshrc/main.zsh
@@ -1,27 +1,10 @@
 
 ZSHRC="$HOME/.dotfiles/zshrc";
 
-# independent environment variables 
-[ -f "$HOME/.environment" ] && source "$HOME/.environment"; 
-# set ls colors
-eval `dircolors $HOME/.dircolors`
-
-alias ls='ls -h --color=auto'
 alias gr='cd $(git root)'
 alias less='less -S' # enable horizontal scrolling in less
 alias s='ssh'
-
-export EDITOR=vim
-
 alias subs='filebot -get-subtitles'
-
-# set virtualenvwrapper working directory
-export WORKON_HOME=$HOME/.virtualenvs
-
-# ZMV
-autoload zmv
-alias zmz='noglob zmv'
-alias zcp='noglob zmv -C'
 
 # oh-my-zsh
 source $ZSHRC/oh-my-zsh.zsh
@@ -31,4 +14,23 @@ source $ZSHRC/pure.zsh
 # OS specific code
 [ `uname` '==' "Linux" ] && source "$ZSHRC/linux.zsh";
 [ `uname` '==' "Darwin" ] && source "$ZSHRC/mac.zsh";
+
+# independent environment variables 
+[ -f "$HOME/.environment" ] && source "$HOME/.environment"; 
+# set ls colors
+eval `dircolors $HOME/.dotfiles/conf/dircolors`
+
+export EDITOR=vim
+
+
+# set virtualenvwrapper working directory
+export WORKON_HOME=$HOME/.virtualenvs
+
+# initialize autoenv
+autoenv_init
+
+# ZMV
+autoload zmv
+alias zmz='noglob zmv'
+alias zcp='noglob zmv -C'
 
