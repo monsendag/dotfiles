fe7d3d9dae14e6eb65123b24b08bf657e4ee07dd
Dag Einar Monsen <me@dag.im>
2014-04-29 13:29:31 +0200

    Temporary fix for plugin history-substring-search (robbyrussel/oh-my-zsh#2735)

--- zshrc/oh-my-zsh.zsh
+++ zshrc/oh-my-zsh.zsh
@@ -3,3 +3,9 @@ ZSH_THEME=""
 
 plugins=(vi-mode gitfast history-substring-search colored-man brew bower common-aliases mvn)
 source $ZSH/oh-my-zsh.sh
+
+# fix for history substring search (see robbyrussel/oh-my-zsh#2735)
+bindkey "^[OA" up-line-or-history
+bindkey "^[OB" down-line-or-history 
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
