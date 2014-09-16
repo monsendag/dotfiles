7d5f338b171d356ae4eaba3f69b2e18c923387e7
Dag Einar Monsen <dagm@mesan.no>
2014-09-16 12:51:39 +0200

    Change default shell in Cygwin from bash to zsh

--- init/cygwin.sh
+++ init/cygwin.sh
@@ -1,6 +1,11 @@
 # Cygwin-only stuff. Abort if not Cygwin.
 [[ "$OSTYPE" =~ ^cygwin ]] || return 1
 
+
+# replace bash with zsh
+perl -pi -w -e 's/bash --login/zsh -l/g;' /Cygwin.bat
+
+
 # Install Cygwin packages.
 if [[ "$(type -P cygwin.exe)" ]]; then
   packages="openssh,subversion,vim,curl,zsh"
