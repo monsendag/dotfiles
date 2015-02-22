14b57b3589d7f0fa25a05a0be03723b1cecd5a6c
Dag <me@dag.im>
2015-02-22 14:28:46 +0100

    fix zsh prompt

--- zshrc/prompt.zsh
+++ zshrc/prompt.zsh
@@ -124,8 +124,25 @@ setup() {
 	# set ls colors
 	eval `dircolors $HOME/.dotfiles/conf/dircolors.conf`
 
-  # enable vi mode
+  # enable vi mode http://dougblack.io/words/zsh-vi-mode.html
   bindkey -v
+
+  bindkey '^P' up-history
+  bindkey '^N' down-history
+  bindkey '^?' backward-delete-char
+  bindkey '^h' backward-delete-char
+  bindkey '^w' backward-kill-word
+  bindkey '^r' history-incremental-search-backward
+
+  function zle-line-init zle-keymap-select {
+      VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+      RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+      zle reset-prompt
+  }
+
+  zle -N zle-line-init
+  zle -N zle-keymap-select
+  export KEYTIMEOUT=1
 
 	# include zsh-syntax-highlighting
 	source ~/.dotfiles/vendor/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
