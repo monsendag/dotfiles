35b4c88b10f4c65f1f175ffe5d0f265217625da8
Dag <me@dag.im>
2015-04-20 12:55:26 +0200

    better date format for ls

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -13,7 +13,7 @@ alias jq='jq -C'
 unalias ls
 alias ls="$(which ls) --color"
 
-alias l='ls -lFh'     #size,show type,human readable
+alias l='ls -lFh -t --time-style=long-iso'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
