bb87af6d720827da8b73f8a2e3e2b3deb39298e0
Dag <me@dag.im>
2016-12-20 22:54:31 +0100

    fix dotfiles

--- bin/utils.bash
+++ bin/utils.bash
@@ -3,3 +3,10 @@ function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
 function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
+
+function test() {
+  e_header "header"
+  e_success "success"
+  e_error "error"
+  e_arrow "arrow"
+}
