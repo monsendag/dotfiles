baabab957d8c8087634a6f03b85004294b2a0902
Dag M <me@dag.im>
2019-08-15 20:02:37 +0200

    mac.zsh: handle java not installed

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -1,6 +1,6 @@
 
 # JAVA_HOME
-export JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME=$(/usr/libexec/java_home 2>/dev/null)
 
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
 
