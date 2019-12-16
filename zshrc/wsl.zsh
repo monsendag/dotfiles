835f60429eb3da06063fc95467f3e031faaeaa49
Dag Einar Monsen <me@dag.im>
2019-12-16 11:51:07 +0100

    wsl: disable nice

--- zshrc/wsl.zsh
+++ zshrc/wsl.zsh
@@ -2,3 +2,7 @@ alias pbcopy='clip.exe'
 alias pbpaste='powershell.exe Get-Clipboard'
 alias 'o.'='explorer.exe .'
 alias killall='taskkill.exe /F /IM'
+
+# nice doesn't work on WSL
+# https://github.com/microsoft/WSL/issues/1887
+unsetopt BG_NICE
