ab3a1053b213d5a618b691d25f41a16bae504b2a
Dag M <me@dag.im>
2020-11-15 00:51:20 +0100

    remove autoenv
    Not using Python much these days...

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -18,15 +18,3 @@ alias rstudio='open -a "Rstudio"'
 alias javarepl='java -jar /usr/local/lib/javarepl.jar'
 alias trs='trash'	
 alias serve='open http://localhost:8080 && static' 
-
-# load virtualenvwrapper
-[ -f /usr/local/bin/virtualenvwrapper.sh ] && source /usr/local/bin/virtualenvwrapper.sh;
-
-# load autoenv
-[ -f /usr/local/opt/autoenv/activate.sh ] && source /usr/local/opt/autoenv/activate.sh;
-
-
-# initialize autoenv
-# autoenv_init
-
-
