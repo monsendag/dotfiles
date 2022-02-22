f6fe7e337bf3c04429438a00260166797a918907
Dag Einar Monsen <dag.einar.monsen@vegvesen.no>
2022-02-22 09:32:29 +0100

    wsl: alias o=open

--- zshrc/wsl.zsh
+++ zshrc/wsl.zsh
@@ -2,6 +2,7 @@ alias pbcopy='clip.exe'
 alias pbpaste='powershell.exe Get-Clipboard'
 alias killall='taskkill.exe /F /IM'
 alias open='wsl-open'
+alias o=open
 
 # nice doesn't work on WSL
 # https://github.com/microsoft/WSL/issues/1887
