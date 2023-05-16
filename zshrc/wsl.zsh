0a2f27040381fa8b2feb0c892ff8e15a51cd3575
Dag Einar Monsen <dag.einar.monsen@vegvesen.no>
2023-05-16 15:06:12 +0200

    wsl.zsh: various aliases

--- zshrc/wsl.zsh
+++ zshrc/wsl.zsh
@@ -4,6 +4,24 @@ alias killall='taskkill.exe /F /IM'
 alias open='wsl-open'
 alias o=open
 
+alias npmw='cmd.exe /C npm'
+alias cmd='cmd.exe /C'
+
 # nice doesn't work on WSL
 # https://github.com/microsoft/WSL/issues/1887
 unsetopt BG_NICE
+
+wslrealpath() {
+  if ( echo $1 | grep -q "^\/mnt\/c\/*" ) 
+  then
+    wslpath -ua "$1"
+  # otherwise we are working on a repo within the WSL VM: use Linux git
+  else
+    realpath "$1"
+  fi
+}
+
+unalias cr 2>/dev/null
+cr() {
+  echo $(wslrealpath $(git root))
+}
