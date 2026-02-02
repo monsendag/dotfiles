76761e9b6fb912f2312f4553160fd422aee179fa
Dag Einar Monsen <me@dag.im>
2026-02-02 20:59:22 +0100

    mac.zsh: remove unused aliases

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -1,7 +1,5 @@
 
 # JAVA_HOME
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-12.jdk/Contents/Home"
-
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH"
 
 # only update homebrew once a week
@@ -9,12 +7,5 @@ export HOMEBREW_AUTO_UPDATE_SECS=604800
 
 # mac specific aliases
 alias g='git'
-alias gu='open -a GitUp'
-alias mou='open -a Mou'
-alias subl='Open -a "Sublime Text 2"'
-alias br='Open -a "Brackets"'
 alias q='qlmanage 2>/dev/null -p'
-alias rstudio='open -a "Rstudio"'
-alias javarepl='java -jar /usr/local/lib/javarepl.jar'
-alias trs='trash'	
 alias bic='brew install --cask'
